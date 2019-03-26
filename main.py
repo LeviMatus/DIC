4a9680c302bfe02dce40de6ce5703e427a358656
levi <thelevimatusproject@gmail.com>
2019-03-25 22:45:12 -0300

    add sliding window iteration.

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ from Node import Node
 import pandas as pd
 
 
-def main():
+def main(m=2):
 
     data = pd.read_csv("Play_Tennis_Data_Set.csv")
     data['Windy'] = data['Windy'].map({True: 'True', False: 'False'})
 
     data1 = list([val for d in data for val in data[d].unique()])
 
-    data2 = data.values
-
     root = Node()
 
+    # Initial pass to build Itemsets of size 1
     for d in data1:
         root.add_child((d,))
 
-    for d in data2:
-        root.increment(d)
+    # Pass over the dataset in m-sized chunks.
+    for i, end in enumerate(range(m, len(data)+m, m)):
+        for d in data[i*m:end].iterrows():
+            root.increment(d[1])
 
     root.to_string(None)
 
